8ba1fbdbd92cc55991f56e1fbf6bd92bc0de6d7c
Daniel <daniel.mergar.1@educa.jcyl.es>
2022-10-10 10:34:30 +0000

    lunes

---
+++ Tema-3/Teoria/EjercicioPiramide.php
@@ -0,0 +1,28 @@
+<h2>Piramide de asteriscos</h2>
+
+<?
+
+
+$filas=5;
+$asterisco=1;
+$espacio=$filas*2;
+for($i=0; $i<$filas;$i++)
+{
+    //espacios
+    for($f=0;$f<=$espacio;$f++)
+    {
+        echo("&nbsp;");
+    }
+    //Asteriscos izquierda
+    for($g=0;$g<$asterisco;$g++)
+    {
+        echo("*");
+    }
+
+   
+    $asterisco+=2;
+    $espacio-=2;
+    echo "<br>";
+}
+
+?>
