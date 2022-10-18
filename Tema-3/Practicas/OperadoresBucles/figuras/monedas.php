e2a83164dbc9ad6cdf1dec198b959c24237ebfc3
Daniel <daniel.mergar.1@educa.jcyl.es>
2022-10-18 18:31:13 +0000

    asd

---
+++ Tema-3/Practicas/OperadoresBucles/figuras/monedas.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio Monedas</title>
+</head>
+<body>
+    <?php
+        $precio= $_GET["precio"];
+        $pago = $_GET["pago"];
+        echo $precio;
+        echo "<br>";
+        echo $pago;
+
+        $dos=2;
+        $uno=1;
+        $cinc=0.50;
+        $vein=0.20;
+        $diez=0.10;
+        $cinco=0.05;
+        $dos=0.02;
+        $uno=0.01;
+    ?>
+</body>
+</html>
